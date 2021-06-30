d34bb190f4be3b668b642059ce23ed6689caeac4
NatanaelGomesCardoso <natangomescardoso0@gmail.com>
2021-06-30 13:58:19 -0300

    created class Pessoa

---
+++ src/main/kotlin/natan/devteste/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package natan.devteste.digionebank
+
+class Pessoa(nome:String , cpf:String) {
+    var nomep : String = nome
+    var cpfp : String = cpf
+}
+
+fun main(){
+    val natan = Pessoa("Natan" ,"072.5698.123-55")
+    println(natan.nomep)
+    println(natan.cpfp)
+}
