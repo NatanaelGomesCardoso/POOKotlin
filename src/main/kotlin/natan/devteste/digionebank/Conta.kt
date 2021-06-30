1a684a27fce2654882387f0936340ae7802a390d
NatanaelGomesCardoso <natangomescardoso0@gmail.com>
2021-06-30 15:14:28 -0300

    uptade : created class Conta

---
+++ src/main/kotlin/natan/devteste/digionebank/Conta.kt
@@ -0,0 +1,18 @@
+package natan.devteste.digionebank
+
+import java.math.BigDecimal
+
+// cria a classe Conta
+class Conta(
+    val agencia: String, // objeto agência
+    val numDaConta: String, // objeto número da conta
+    val saldoDaconta: BigDecimal // objeto saldo da conta
+) {
+    fun deposito(valor: BigDecimal){ // função de depósito
+
+    }
+
+    fun saque(valor: BigDecimal){ // função de saque
+
+    }
+}
